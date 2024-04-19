0808121f8fc1a450e9296e9db79530205cfecd35
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-04-19 16:58:01 +0500

    i learn about map object how to create map object, how to use map object has() method,set() method and foreach loop to iterate map object.

---
+++ index.ts
@@ -0,0 +1,37 @@
+// Question 112
+let countries = new Map<string,string>();
+countries.set("Pakistan","Islamabad");
+countries.set("France","Paris");
+countries.set("Saudia Arabia","Riyadh");
+countries.set("Canda","Ottawa");
+
+console.log(countries);
+
+// Question 113
+// function logCapitalofCanda(countries:Map<string,string>):void{
+//     if(countries.has("Canda")){
+//         console.log(`The capital of canda is ${countries.get("Canda")}`);
+//     }
+//     else{
+//         console.log("Canda is not in the map.");
+//     }
+// }
+// logCapitalofCanda(countries);
+
+if(countries.has("Canda")){
+    console.log(`The capital of canda is ${countries.get("Canda")}`);
+}
+else{
+    console.log("Canda is not in the map.");
+}
+
+// Question 114
+let Student = new Map<string,number>();
+Student.set("Maham",809);
+Student.set("Mahnoor",803);
+Student.set("Sabar",805);
+
+Student.forEach((id,name)=>{
+    console.log(`Student name is ${name} and Student id is ${id}`);
+    // console.log(`Student ID: ${id}, Name: ${name}`);
+})
